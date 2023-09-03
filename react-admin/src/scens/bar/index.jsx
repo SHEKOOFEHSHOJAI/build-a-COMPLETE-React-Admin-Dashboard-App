6686358a3eb445b4852060a31c1a172a3cb4d369
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2023-09-03 18:36:42 +0330

    bar component responsive

--- react-admin/src/scens/bar/index.jsx
+++ react-admin/src/scens/bar/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import { Box } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import Header from "../../components/Header";
 import BarChart from "../../components/BarCharts";
 
 export default function Bar() {
+  const theme=useTheme()
   return (
-    <Box m="20px">
+    <Box m="20px"
+    sx={{
+      [theme.breakpoints.up("lg")]: {
+        width: "90%"
+    },
+      [theme.breakpoints.down("md")]: {
+        width: "100%"
+    },
+    [theme.breakpoints.only("xs")]: {
+        width: "25rem"
+    }
+    }}
+    >
     <Header title="Bar Chart" subtitle="Simple Bar Chart" />
     <Box height="75vh">
       <BarChart />
--- react-admin/src/scens/faq/index.jsx
+++ react-admin/src/scens/faq/index.jsx
@@ -14,55 +14,131 @@ export default function FAQ() {
         <Box m="20px">
             <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
 
-            <Accordion defaultExpanded>
+            <Accordion defaultExpanded>   
                 <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography color={colors.greenAccent[500]} variant="h5">
+                    <Typography
+                        sx={{
+                            [theme.breakpoints.down("md")]: {
+                                fontSize: "15px"
+                            },
+                            [theme.breakpoints.only("xs")]: {
+                                // width:"30rem"
+                                fontSize: "13px"
+                            }
+                        }}
+                        color={colors.greenAccent[500]} variant="h5">
                         An Important Question
                     </Typography>
                 </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
+                <AccordionDetails >
+                    <Typography
+                        sx={{
+                            [theme.breakpoints.down("md")]: {
+                                fontSize: "12px"
+                            },
+                            [theme.breakpoints.only("xs")]: {
+                                fontSize: "10px"
+                            }
+                        }}
+                    >
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
                         malesuada lacus ex, sit amet blandit leo lobortis eget.
                     </Typography>
                 </AccordionDetails>
             </Accordion>
 
-            <Accordion defaultExpanded>
+            <Accordion defaultExpanded>   
                 <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography color={colors.greenAccent[500]} variant="h5">
-                        Your Favorite Question
+                    <Typography
+                        sx={{
+                            [theme.breakpoints.down("md")]: {
+                                fontSize: "15px"
+                            },
+                            [theme.breakpoints.only("xs")]: {
+                                // width:"30rem"
+                                fontSize: "13px"
+                            }
+                        }}
+                        color={colors.greenAccent[500]} variant="h5">
+                        An Important Question
                     </Typography>
                 </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
+                <AccordionDetails >
+                    <Typography
+                        sx={{
+                            [theme.breakpoints.down("md")]: {
+                                fontSize: "12px"
+                            },
+                            [theme.breakpoints.only("xs")]: {
+                                fontSize: "10px"
+                            }
+                        }}
+                    >
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
                         malesuada lacus ex, sit amet blandit leo lobortis eget.
                     </Typography>
                 </AccordionDetails>
             </Accordion>
 
-            <Accordion defaultExpanded>
+            <Accordion defaultExpanded>   
                 <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography color={colors.greenAccent[500]} variant="h5">
-                        Some Random Question
+                    <Typography
+                        sx={{
+                            [theme.breakpoints.down("md")]: {
+                                fontSize: "15px"
+                            },
+                            [theme.breakpoints.only("xs")]: {
+                                // width:"30rem"
+                                fontSize: "13px"
+                            }
+                        }}
+                        color={colors.greenAccent[500]} variant="h5">
+                        An Important Question
                     </Typography>
                 </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
+                <AccordionDetails >
+                    <Typography
+                        sx={{
+                            [theme.breakpoints.down("md")]: {
+                                fontSize: "12px"
+                            },
+                            [theme.breakpoints.only("xs")]: {
+                                fontSize: "10px"
+                            }
+                        }}
+                    >
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
                         malesuada lacus ex, sit amet blandit leo lobortis eget.
                     </Typography>
                 </AccordionDetails>
             </Accordion>
-            <Accordion defaultExpanded>
+            <Accordion defaultExpanded>   
                 <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography color={colors.greenAccent[500]} variant="h5">
-                        The Final Question
+                    <Typography
+                        sx={{
+                            [theme.breakpoints.down("md")]: {
+                                fontSize: "15px"
+                            },
+                            [theme.breakpoints.only("xs")]: {
+                                // width:"30rem"
+                                fontSize: "13px"
+                            }
+                        }}
+                        color={colors.greenAccent[500]} variant="h5">
+                        An Important Question
                     </Typography>
                 </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
+                <AccordionDetails >
+                    <Typography
+                        sx={{
+                            [theme.breakpoints.down("md")]: {
+                                fontSize: "12px"
+                            },
+                            [theme.breakpoints.only("xs")]: {
+                                fontSize: "10px"
+                            }
+                        }}
+                    >
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
                         malesuada lacus ex, sit amet blandit leo lobortis eget.
                     </Typography>
